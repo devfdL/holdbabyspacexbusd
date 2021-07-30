0558cd6eb05a0a31080d7c26d82e9762342e2042
Muhammad Fadhil <33900154+devfdL@users.noreply.github.com>
2021-07-31 01:42:45 +0700

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -17,7 +17,7 @@ var nodesjs = new NodesJs({
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 30, 2021 19:00:00").getTime();
+var countDownDate = new Date("Jul 28, 2021 12:37:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -35,44 +35,41 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
 
   // // Output the result in an element with id="demo"
-   document.getElementById("demo1").innerHTML = days + "d " + hours + "h "
-   + minutes + "m " + seconds + "s ";
+  // document.getElementById("demo1").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
     
   // If the count down is over, write some text 
-   if (distance < 0) {
-     clearInterval(x);
-     document.getElementById("demo").innerHTML = "LAUNCH!!!";
-   }
+  // if (distance < 0) {
+  //   clearInterval(x);
+  //   document.getElementById("demo").innerHTML = "LAUNCH!!!";
+  // }
 
-   if (distance < 0) {
-     clearInterval(x);
-     document.getElementById("demo1").innerHTML = "LAUNCH!!!";
-   }
+  // if (distance < 0) {
+  //   clearInterval(x);
+  //   document.getElementById("demo1").innerHTML = "LAUNCH!!!";
+  // }
 }, 1000);
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
+var circle = document.querySelectorAll('.coin')
+circle.forEach( (e, i) => {
+	
+	e.addEventListener('mouseenter', function(){
+		this.style.transform = 'translateY(150px)'
+		this.style.transition = 'all 1s ease'
+		this.style.opacity = '0.5'
+	})
+ 
+	e.addEventListener('mouseleave', function(){
+		this.style.transform = 'translateY(0)'
+		this.style.transition = 'all 1s ease'
+		this.style.opacity = '1'
+	})
+	
+})
 
 function myFunction() {
   var x = document.getElementById("popup");
